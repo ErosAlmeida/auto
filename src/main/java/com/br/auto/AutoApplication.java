00ddaf784fbeb3c9e2211737db4df23c2718ac6a
Eros <eros.almeida.vilela@gmail.com>
2026-09-12 11:45:33 -0400

    poo

--- src/main/java/com/br/auto/AutoApplication.java
+++ src/main/java/com/br/auto/AutoApplication.java
@@ -1,5 +1,6 @@
 package com.br.auto;
 
+import com.br.auto.Pessoa.Pessoa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/br/auto/Pessoa/Aplicacao.java
@@ -0,0 +1,13 @@
+package com.br.auto.Pessoa;
+
+public class Aplicacao {
+    /*
+    Pessoa p1 = new Pessoa();
+    p1.nome = "jose";
+
+    p1.receber(5)
+    print (p1.nome)
+
+
+    * */
+}
